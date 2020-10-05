e173fe58d8fb5f430ba9162516a87b782f0b1e51
slubert <oliver.tallgard@gmail.com>
2020-10-05 10:43:43 +0300

    finniched

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 const output = document.getElementById("output");
 const cityInput = document.getElementById("cityInput");
-const submit = document.getElementById("form")
+const submit = document.getElementById("form");
+const background = document.getElementById("background");
+const feelsLikeOutput = document.getElementById("feelsLikeOutput")
 
 submit.addEventListener("submit", onSubmit)
 
 function onSubmit(e){
-    e.preventDefault
+    e.preventDefault();
     let place = cityInput.value;
     
     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + place + "&appid=c84bdf644399cebe626d9897429823dd&units=metric")
@@ -13,14 +15,29 @@ function onSubmit(e){
         .then(whenLoaded)
     
     function whenLoaded(data){
-        output.textContent = data.main.temp
-        
-        console.log(data)
-        console.log(data.main.temp)
+        output.textContent = data.main.temp.toFixed(0) + "°C"
+        feelsLikeOutput.textContent = "feels like " + data.main.feels_like.toFixed(0) + "°C"
+
+        if(data.weather[0].main === "Clouds"){
+            background.style.backgroundImage = "url('pics/clowdy.jpg')"
+            output.style.color = "white"
+            feelsLikeOutput.style.color = "White"
+        }
+        else if(data.weather[0].main === "Rain"){
+            background.style.backgroundImage = "url('pics/rainny.jpg')"
+            output.style.color = "white"
+            feelsLikeOutput.style.color = "White"
+        }
+        else if(data.weather[0].main === "Sun"){
+            background.style.backgroundImage = "url('pics/sunny.jpg')"
+            output.style.color = "white"
+            feelsLikeOutput.style.color = "White"
+        }
+        else if(data.weather[0].main === "Clear"){
+            background.style.backgroundImage = "url('pics/clear.jpg')"
+        }
     }
 }
-
-
 
 
 // https://openweathermap.org/current#zip
